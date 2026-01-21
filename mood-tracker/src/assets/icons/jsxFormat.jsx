f24d7dd3d07296bca1f3f8957dc73b4386e80e08
Matt <matt4real@yahoo.com>
2026-01-21 22:06:35 +0100

    Remove mood-tracker (moved to its own repo)

--- mood-tracker/src/assets/icons/jsxFormat.jsx
+++
@@ -1,8 +0,0 @@
-function Icon({size=45}) {
-  // width={size} height={size}
-  return (
-    
-  )
-}
-
-export default Icon
--- mood-tracker/src/components/Layout/PageWrapper/PageWrapper.js
+++
@@ -1,7 +0,0 @@
-import './pageWrapper.css'
-
-function PageWrapper({ children }) {
-  return <main className="page-wrapper">{children}</main>;
-}
-
-export default PageWrapper;
